1e48fdb7fab2f0d2fa0ac64db85e7bf277526c1a
philipp_sciscry <philipp@sciscry.ai>
2021-02-05 00:50:08 +0100

    adding string character handling example

--- basics-io.c
+++ basics-io.c
@@ -11,5 +11,16 @@ int main()
   printf("%s World\n", "Hello");
   printf("X: %d, Y: %d\n", x, y);
   printf("The value of x + y is %d\n", x+y);
+
+  int radius;
+  printf("Give me a radius: ");
+  scanf("%d", &radius); // address-of-operator (pointer)
+  printf("You chose the value %d\n", radius);
+
+
+  char name[10]; // char array of 20 characters. '\0' takes one spot
+  printf("Give me your name: ");
+  scanf("%9s", name);
+  printf("Your name: %s\n", name);
   return 0;
 }
