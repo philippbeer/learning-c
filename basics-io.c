9582f6e2034ce018770af0702f42bf6e0411f0f8
philipp_sciscry <philipp@sciscry.ai>
2021-02-05 00:35:49 +0100

    adding some printing functionality

--- basics-io.c
+++ basics-io.c
@@ -7,6 +7,9 @@ int main()
   int x = 50;
   int y; // declaration
   y = 42; // initialization
-   
+
+  printf("%s World\n", "Hello");
+  printf("X: %d, Y: %d\n", x, y);
+  printf("The value of x + y is %d\n", x+y);
   return 0;
 }
