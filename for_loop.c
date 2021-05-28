4218c226b920e203e08a58f14bb662d95b902ece
Philipp Beer <philipp@sciscry.ai>
2021-05-28 22:13:19 +0200

    adding symbolic constants

--- for_loop.c
+++ for_loop.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0    /* lower limit of table */
+#define UPPER 300  /* upper limit */
+#define STEP 20    /* step size */
+
 /* print Fahrenheit-Celsius table */
 int main()
 {
   int fahr;
 
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
     printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
