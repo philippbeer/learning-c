8e24bc3b703f7e195a781b51cc3326b7d7108662
Philipp Beer <philipp@sciscry.ai>
2021-05-28 16:58:58 +0200

    update temperatures

--- fahrenheit2celsius.c
+++ fahrenheit2celsius.c
@@ -7,14 +7,26 @@ int main()
   int fahr, celsius;
   int lower, upper, step;
 
-  lower = 0;    /* lower limit of temperature table */
+  lower = -40;    /* lower limit of temperature table */
   upper = 300;  /* upper limit */
   step = 20;    /* step size */
 
   fahr = lower;
+  printf("Fahrenheit -> Celsius\n\n");
+  printf("Fahrenheit\tCelsius\n");
   while (fahr <= upper) {
     celsius = 5 * (fahr-32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    printf("%d\t\t%d\n", fahr, celsius);
     fahr = fahr + step;
+  }
+
+  // Converting Celsius to Fahrenheit
+  celsius = lower;
+  printf("Celsius -> Fahrenheit\n\n");
+  printf("Celsius\tFahrenheit\n");
+  while (celsius <= upper) {
+    fahr = celsius * 9/5 + 32;
+    printf("%d\t%d\n", celsius, fahr);
+    celsius = celsius + step;
   }
 }
