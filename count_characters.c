6842e96b86bcbca16b91788e4e89503b2ac8981e
Philipp Beer <philipp@sciscry.ai>
2021-05-30 20:38:46 +0200

    modify int to double and while to for loop

--- count_characters.c
+++ count_characters.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input; 2nd version */
 int main()
 {
-  long nc;
+  double nc;
 
-  nc = 0;
-  while (getchar() != EOF)
-    ++nc;
-  printf("%1ld\n", nc);
+  for (nc = 0; getchar() != EOF; ++nc)
+    ;
+  printf("%.0f\n", nc);
+
 }
