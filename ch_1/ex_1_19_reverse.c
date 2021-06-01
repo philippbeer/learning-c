6e7eb841686bc59b7a2a01d43ebc0c3efd6e1774
Philipp Beer <philipp@sciscry.ai>
2021-06-01 18:00:46 +0200

    adding exercises ch 1.9

---
+++ ch_1/ex_1_19_reverse.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum input line length */
+#define LONGLINE 80  /* threshold for long lines */
+
+int get_line(char line[], int maxline);
+int reverse_line(char line[]);
+void copy(char to[], char from[]);
+
+ /* print the longest input line */
+int  main()
+ {
+   int len; /* current line length */
+   char line[MAXLINE]; /* current input line */
+   
+   while ((len = get_line(line, MAXLINE)) > 0)
+     if (len > LONGLINE)
+       printf("%s", line);
+
+   return 0;
+ }
+
+
+ /* getline: read a line into s, return length */
+ int get_line(char s[],int lim)
+ {
+   int c, i;
+   for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+     s[i] = c;
+   if (c == '\n') {
+     s[i] = c;
+     ++i;
+   }
+   s[i] = '\0';
+   return i;
+ }
